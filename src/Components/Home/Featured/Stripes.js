cb00fd884306724d1ff1447c66464fb8de2cecda
Shakil2015 <sshakil496@gmail.com>
2020-01-19 01:02:07 +0600

    NotUpdated

--- src/Components/Home/Featured/Stripes.js
+++ src/Components/Home/Featured/Stripes.js
@@ -3,30 +3,53 @@ import {easePolyOut} from 'd3-ease';
 import Animate from 'react-move/Animate';
 
 export default class Stripes extends Component {
-    constructor(props){
-        super(props);
-        this.state={
-            stripes:[
-                {
-                    background:'#8DC0E6'
-                },
-                {
-                    background:'#ffffff'
-                },
-                {
-                    background:'#8DC0E6'
-                }
-            ]
-        }
-
+   constructor(props){
+       super(props);
+       this.state={
+        stripes:[
+            {
+                background:'#8DC0E6'
+            },
+            {
+                background:'#ffffff'
+            },
+            {
+                background:'#8DC0E6'
+            }
+        ]
     }
+   }
     
-     showStripes =()=>{
+
+    
+    
+   
+     showStripes =()=>(
        this.state.stripes.map((stripe,i)=>(
-           
+           <Animate
+            key={i}
+            show={true}
+            start={{
+                background:'#ffffff'
+            }}
+            enter={{
+                background:[this.state.stripes.background]
+            }}
+           >
+               {({background})=>{
+                   return(
+                       <div
+                       className="stripe"
+                       style={{
+                           background
+                       }}
+                       ></div>
+                   );
+               }}
+           </Animate>
 
        ))
-    }
+     )
          
     
     render() {
