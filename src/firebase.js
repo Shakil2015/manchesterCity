fd88f9a4682c551eeda52e0a1b7c1fa4489e1c63
Shakil2015 <sshakil496@gmail.com>
2020-01-22 18:46:36 +0600

    firebaseOffice22jan

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app';
+import 'firebase/app';
+import 'firebase/database'
+var firebaseConfig = {
+    apiKey: "AIzaSyDFiZu5xjlJZKdHbULL03enPXntGoaUdzQ",
+    authDomain: "mancity-9f80f.firebaseapp.com",
+    databaseURL: "https://mancity-9f80f.firebaseio.com",
+    projectId: "mancity-9f80f",
+    storageBucket: "mancity-9f80f.appspot.com",
+    messagingSenderId: "764926267309",
+    appId: "1:764926267309:web:de1120c97e38d159d05b24",
+    measurementId: "G-01LSRJSN8G"
+  };
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
